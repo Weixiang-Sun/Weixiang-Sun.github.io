97201b808274dfa93a9b29766d5efb47d0ba6df0
Weixiang-Sun <kennysweson@gmail.com>
2025-08-28 03:14:54 +0000

    Deploying to gh-pages from @ Weixiang-Sun/Weixiang-Sun.github.io@92822ef36aeeb8c0771b1130ede578c0c8362327 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,13 +462,6 @@ ninja.data = [{
           window.open("https://github.com/Weixiang-Sun", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
