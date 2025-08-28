57e0fd2d306d7f3b9a75695d59e6574ec9bab65c
Weixiang-Sun <kennysweson@gmail.com>
2025-08-28 03:13:16 +0000

    Deploying to gh-pages from @ Weixiang-Sun/Weixiang-Sun.github.io@784976cf242434eaa692ce95ae20092324cc0745 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,7 +452,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%73%75%6E%34@%6E%64.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Weixiang-Sun", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -460,6 +467,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/weixiang-sun-76641a263", "_blank");
         },
       },{
         id: 'social-rss',
@@ -473,14 +487,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=zizf0i0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
