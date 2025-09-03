1259a72e79f1ca0bcfa2cf7fbb46bbec13e66e81
Weixiang-Sun <kennysweson@gmail.com>
2025-09-03 02:03:50 +0000

    Deploying to gh-pages from @ Weixiang-Sun/Weixiang-Sun.github.io@40151150e0e1cf4e0209ce4954cf4ddaaef9240a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-excited-to-start-my-ph-d-at-university-of-notre-dame",
+          title: 'Excited to start my Ph.D. at University of Notre Dame.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
