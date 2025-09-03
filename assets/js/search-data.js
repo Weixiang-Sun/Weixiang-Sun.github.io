2171a2b417b362f1faad4077de1ba6b39d5f73e9
Weixiang-Sun <kennysweson@gmail.com>
2025-09-03 01:50:27 +0000

    Deploying to gh-pages from @ Weixiang-Sun/Weixiang-Sun.github.io@5bce4106804720e32ef76bf3dd24ab5a87a896ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
